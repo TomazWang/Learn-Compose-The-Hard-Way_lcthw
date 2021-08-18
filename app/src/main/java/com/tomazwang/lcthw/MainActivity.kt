925daa262bf134377490ba30244863e8a8603ccb
Tomaz Wang <TomazWang.tw@gmail.com>
2021-08-19 01:27:03 +0800

    feat: search box with rounded corner

--- app/src/main/java/com/tomazwang/lcthw/MainActivity.kt
+++ app/src/main/java/com/tomazwang/lcthw/MainActivity.kt
@@ -3,19 +3,25 @@ package com.tomazwang.lcthw
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tomazwang.lcthw.ui.theme.LearnComposeTheHardWayTheme
+import com.tomazwang.lcthw.ui.theme.SpotiColor
 import com.tomazwang.lcthw.views.screen.SpotiSearchScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SpotiSearchScreen()
+            Surface(color = SpotiColor.Black, modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+                SpotiSearchScreen()
+            }
         }
     }
 }
--- app/src/main/java/com/tomazwang/lcthw/views/screen/SpotiSearchScreen.kt
+++ app/src/main/java/com/tomazwang/lcthw/views/screen/SpotiSearchScreen.kt
@@ -9,11 +9,18 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,7 +53,7 @@ fun SpotiSearchScreen() {
         )
         
         
-        Box { /* TODO  SearchBar() */ }
+        SearchBox(hintText = "Artists, songs, or podcasts")
         
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Your top generes", color = Color.White, fontWeight = FontWeight.Bold)
@@ -57,6 +64,34 @@ fun SpotiSearchScreen() {
         Text(text = "Browse all", color = Color.White, fontWeight = FontWeight.Bold)
         
         Box {  /* TODO  Blocks() */ }
+    }
+}
+
+
+@Composable
+private fun SearchBox(hintText: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .padding(vertical = 4.dp)
+            .then(modifier)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(4.dp))
+                .background(color = Color.White)
+                .padding(8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "search_icon",
+                tint = SpotiColor.LightGray
+            )
+            // TextField(value = text, onValueChange = { text = it })
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = hintText, color = SpotiColor.LightGray)
+        }
     }
 }
 
